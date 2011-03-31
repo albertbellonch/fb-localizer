355021679768b4643b6dad9b6f55129587ea9965
Albert Bellonch <albert@itnig.net>
2011-03-31 23:38:54 +0200

    Solved some style stuff

--- lib/fb-localizer.rb
+++ lib/fb-localizer.rb
@@ -4,12 +4,12 @@ require 'i18n'
 module FbLocalizer
   module FbLocalizerHelpers
     def get_fb_locale(ruby_locale = nil)
-      ruby_locale = ruby_locale || I18n.locale
+      ruby_locale ||= I18n.locale
       fb_candidate_locales = FbLocalizer::FbLocalizerHelpers::FB_LOCALES.select{ |l| l.match(/^#{ruby_locale}/)}
-      if !fb_candidate_locales.empty?
-        return fb_candidate_locales[0]
+      if fb_candidate_locales.any?
+        fb_candidate_locales[0]
       else
-        return "en_US"
+        "en_US"
       end
     end
   end
