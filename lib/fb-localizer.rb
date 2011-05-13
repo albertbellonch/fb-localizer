ff5d13380554e5725b910c9326a36270fb4bc020
Albert Bellonch <albert@itnig.net>
2011-05-13 12:14:20 +0200

    Accepting priorizations

--- lib/fb-localizer.rb
+++ lib/fb-localizer.rb
@@ -2,16 +2,32 @@ require 'fb-localizer/railtie' if defined? Rails
 require 'i18n'
 
 module FbLocalizer
+  DEFAULT_PRIORITIES = { :en => "en_US", :es => "es_ES", :pt => "pt_PT", :zh => "zh_CN" }
+  mattr_accessor :priorized
+
   module FbLocalizerHelpers
     def get_fb_locale(ruby_locale = nil)
       ruby_locale ||= I18n.locale
       fb_candidate_locales = FbLocalizer::FbLocalizerHelpers::FB_LOCALES.select{ |l| l.match(/^#{ruby_locale}/)}
       if fb_candidate_locales.any?
-        fb_candidate_locales[0]
+        priorized = FbLocalizer.priorized[ruby_locale] # get priorized locale for that one
+        if priorized && fb_candidate_locales.include?(priorized) # if available, get that one
+          priorized
+        else # if not, just get the first available
+          fb_candidate_locales[0]
+        end
       else
         "en_US"
       end
     end
+  end
+
+  def self.configure
+    yield self
+  end
+
+  def self.priorized
+    DEFAULT_PRIORITIES.merge(@@priorized)
   end
 end
 
