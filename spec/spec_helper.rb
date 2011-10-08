8ca85ac915b71e4e7379f654ea2de0259de5ed27
Albert Bellonch <albert@itnig.net>
2011-10-08 16:49:05 +0200

    Specify environment in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # Require fb-localize and load server
+ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../testapp/config/environment', __FILE__)
 require 'rspec/rails'
 require 'fb-localizer'
