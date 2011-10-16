e28889b49bffdf9cb6898622e63d63565130c09b
Albert Bellonch <albert@itnig.net>
2011-10-16 12:35:33 +0200

    Updating to Rails 3.1

--- testapp/config/initializers/secret_token.rb
+++ testapp/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Testapp::Application.config.secret_token = 'ae50384afc2ad02d2205ee375bf178e4e27513c2533b0d54721c6545472ffbe3db104de81e1bbe0cee53f5e4ebaaaf9353660802ccc7b64460d9569bf97d09fc'
+Testapp::Application.config.secret_token = '848de198d948b8320381d48ad49b3e43fb295bcdf8093321ddee8f21ab6dd38acba4a3047a407f1c6025adcc08262696c54c1298e93ab0409e2734e90f258f96'
